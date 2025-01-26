de92f91aef3fdf766aec4f76f89e721d991a451f
bcier <b.ciereszynski@wp.pl>
2025-01-26 23:36:08 +0100

    add compute method prototype

--- algorithm/gmm.cpp
+++ algorithm/gmm.cpp
@@ -1,5 +1,8 @@
 #include "GMM.h"
+#include <chrono>
 #include <iostream>
+#include <limits>
+
 #include "sampling.h"
 
 namespace {
@@ -43,6 +46,54 @@ GMMResult GMM::fit(const std::vector<std::vector<double>>& data, GMMResult& resu
     if (verbose) {
         std::cout << "Starting GMM...\n";
     }
+    const auto start_time = std::chrono::system_clock::now();
+    const int n = static_cast<int>(data.size());
+    const int d = static_cast<int>(data[0].size());
+    const int k = static_cast<int>(result.clusters.size());
+
+    result.iterations = max_iterations;
+    result.converged = false;
+    double result_objective = -std::numeric_limits<double>::infinity();
+    double previous_objective;
+
+    auto log_responsibilities = std::vector<std::vector<double>>(n, std::vector<double>(k, 0.0));
+    auto precision_cholesky = std::vector<std::vector<std::vector<double>>>(k,
+        std::vector<std::vector<double>>(d, std::vector<double>(d, 0.0)));
+    // compute cholesky
+
+    if (n == k) {
+        result.converged = true;
+        result.iterations = 1;
+    }
+    else for (int i = 0; i<max_iterations; ++i) {
+        previous_objective = result_objective;
+
+        const auto t0 = std::chrono::system_clock::now();
+        // expectation
+        // maximisation
+        const auto t1 = std::chrono::system_clock::now();
+
+        const auto change = result_objective - previous_objective;
+
+        if (verbose) {
+            std::cout << "Iteration " << i + 1 << ": ";
+            std::cout << "Change:" << change << "\t";
+            std::cout << "Objective:" << result.objective << "\t";
+            std::cout << "Time: " << t1-t0 << "\t";
+            std::cout << std::endl;
+        }
+
+        if (change < tolerance) {
+            result.converged = true;
+            result.iterations = i;
+            break;
+        }
+
+    }
+
+    // assign points to clusters
+
+    result.elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::system_clock::now() - start_time).count();
     return result;
 }
 
@@ -59,8 +110,6 @@ GMMResult GMM::fit(const std::vector<std::vector<double>> &data, const int k) {
     assert(k > 0);
     assert(n >= k);
 
-    //try sampling
-    //initialize
     auto [sampled_data, indices] =  try_sampling_unique_data(rng, data, k);
     initialize(result, sampled_data, indices, verbose);
     fit(data, result);
@@ -81,6 +130,6 @@ GMMResult GMM::fit(const std::vector<std::vector<double>> &data, const std::vect
     assert(n >= k);
 
     initialize(result, data, initial_clusters, verbose);
-    GMM::fit(data, result);
+    fit(data, result);
     return result;
 }
