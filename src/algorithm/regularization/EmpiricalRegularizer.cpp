9718a750a569911aecba7de789248144b4059ebd
bcier <b.ciereszynski@wp.pl>
2025-05-14 14:49:20 +0200

    bugfix - regularizer

--- src/algorithm/regularization/EmpiricalRegularizer.cpp
+++ src/algorithm/regularization/EmpiricalRegularizer.cpp
@@ -4,11 +4,17 @@
 std::pair<DoubleMatrix, DoubleVector> EmpiricalRegularizer::fit(
     const DoubleMatrix &data, const std::vector<double> &weights) {
     const auto n = data.rows();
+    const auto d = data.cols();
 
     Eigen::VectorXd mu = get_mu(data, weights);
 
-    DoubleMatrix translated = data.rowwise() - mu.transpose();
-    DoubleMatrix covariance = (translated.transpose() * translated) / n;
+    auto sum_weights = std::accumulate(weights.begin(), weights.end(), 0.0);
+    DoubleMatrix covariance = Eigen::MatrixXd::Zero(d, d);
+    for (int i = 0; i < n; ++i) {
+        Eigen::VectorXd diff = data.row(i).transpose() - mu;
+        covariance += weights[i] * (diff * diff.transpose());
+    }
+    covariance /= sum_weights;
 
     return {covariance, mu};
 }
