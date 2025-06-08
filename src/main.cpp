fb0e74676ae77459e896bbc42351f66f9d0f8f3d
bcier <b.ciereszynski@wp.pl>
2025-06-08 18:01:03 +0200

    modify main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <filesystem>
 
 #include "data.h"
 #include "algorithm/GeneticalAlgorithm.h"
 #include "algorithm/gmm/gmm.h"
 #include "algorithm/gmm/gmmResult.h"
 
+
+namespace fs = std::filesystem;
+
+void print_result(GMMResult result) {
+    // std::cout << "Clusters:" << std::endl;
+    // std::cout << result.clusters << std::endl;
+    std::cout << result.objective << " " << result.iterations << " " << result.elapsed << std::endl;
+
+    // for (const int assignment: result.assignments) {
+    //     std::cout << assignment << std::endl;
+    // }
+}
+
 int main(const int argc, char *argv[]) {
-    std::string path = R"(test_data/3_2_-0.26_1.csv)";
+    std::string directory = "test_data";
     int k = 2;
     int d = 2;
     int n = 100;
-    int seed = 123;
+    int seed = 42;
 
     for (int i = 1; i < argc; i++) {
         if (std::string arg = argv[i]; arg == "--k" && i + 1 < argc) {
             k = std::stoi(argv[++i]);
         } else if (arg == "--path" && i + 1 < argc) {
-            path = argv[++i];
+            directory = argv[++i];
             std::cerr << "Path entered - other options will be ignored" << std::endl;
             break;
         } else if (arg == "--n" && i + 1 < argc) {
@@ -32,24 +46,27 @@ int main(const int argc, char *argv[]) {
     std::vector<std::vector<double> > data;
     std::vector<int> expected_clusters;
 
-    if (path.empty()) {
-        return 1;
-    } else {
-        data = load_data_from_file(path, expected_clusters, k);
-    }
-
-    GeneticalAlgorithm ga{std::mt19937(seed), 200, 150, 40, 50, false};
+    GeneticalAlgorithm ga{std::mt19937(seed), 10, 150, 40, 50, false};
+    data = load_data_from_file(R"(data/3_2_-0.26_1.csv)", expected_clusters, k);
     const auto result = ga.run(data, k);
+    print_result(result);
+    for (const auto &entry: fs::directory_iterator(directory)) {
+        if (!entry.is_regular_file() || entry.path().extension() != ".csv") {
+            continue;
+        }
+        std::string path = entry.path().string();
+        data = load_data_from_file(path, expected_clusters, k);
 
-    std::cout << "Clusters:" << std::endl;
-    std::cout << result.clusters << std::endl;
-    std::cout << result.iterations << std::endl;
-    std::cout << result.elapsed << std::endl;
-    std::cout << result.objective << std::endl;
+        GMMResult result_i(d, data.size(), k);
+        std::cout << path << " ";
+        try {
+            result_i = ga.run(data, k);
+        } catch (std::exception &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
 
-    // for (const int assignment: result.assignments) {
-    //     std::cout << assignment << std::endl;
-    // }
+        print_result(result_i);
+    }
 
     return 0;
 }
