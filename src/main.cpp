513125c87928cc41c4f97775b635d26d9daa68c2
bcier <b.ciereszynski@wp.pl>
2025-06-09 12:03:43 +0200

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -22,23 +22,15 @@ void print_result(GMMResult result) {
 
 int main(const int argc, char *argv[]) {
     std::string directory = "test_data";
-    int k = 2;
-    int d = 2;
-    int n = 100;
-    int seed = 42;
+    int k;
+    int seed = 123;
 
     for (int i = 1; i < argc; i++) {
-        if (std::string arg = argv[i]; arg == "--k" && i + 1 < argc) {
-            k = std::stoi(argv[++i]);
-        } else if (arg == "--path" && i + 1 < argc) {
+        std::string arg = argv[i];
+        if (arg == "--path" && i + 1 < argc) {
             directory = argv[++i];
-            std::cerr << "Path entered - other options will be ignored" << std::endl;
-            break;
-        } else if (arg == "--n" && i + 1 < argc) {
-            n = std::stoi(argv[++i]);
-        } else if (arg == "--d" && i + 1 < argc) {
-            d = std::stoi(argv[++i]);
-        } else if (arg == "--seed" && i + 1 < argc) {
+        }
+        if (arg == "--seed" && i + 1 < argc) {
             seed = std::stoi(argv[++i]);
         }
     }
@@ -46,7 +38,8 @@ int main(const int argc, char *argv[]) {
     std::vector<std::vector<double> > data;
     std::vector<int> expected_clusters;
 
-    GeneticalAlgorithm ga{std::mt19937(seed), 10, 150, 40, 50, false};
+    GeneticalAlgorithm ga{std::mt19937(seed), 200, 150, 40, 50, false};
+    std::cout << "precompile ";
     data = load_data_from_file(R"(data/3_2_-0.26_1.csv)", expected_clusters, k);
     const auto result = ga.run(data, k);
     print_result(result);
@@ -59,6 +52,7 @@ int main(const int argc, char *argv[]) {
 
         GMMResult result_i(d, data.size(), k);
         std::cout << path << " ";
+        GMMResult result_i(data[0].size(), data.size(), k);
         try {
             result_i = ga.run(data, k);
         } catch (std::exception &e) {
