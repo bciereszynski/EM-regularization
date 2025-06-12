ecd49b9e6f414f119047265c944e92ed977f0a5a
bcier <b.ciereszynski@wp.pl>
2025-06-12 16:54:19 +0200

    optimize gaussian parameters compute

--- src/algorithm/gmm/helpers/mathematical.cpp
+++ src/algorithm/gmm/helpers/mathematical.cpp
@@ -20,19 +20,20 @@ Eigen::MatrixXd estimate_weighted_log_probabilities(
     const int d = data.cols();
 
     Eigen::MatrixXd log_probabilities(n, k);
+    const double constant_term = -0.5 * d * std::log(2.0 * M_PI);
 
     for (int i = 0; i < k; ++i) {
         const Eigen::VectorXd mean = result.clusters.row(i).transpose();
-        const Eigen::MatrixXd &cholesky = precisionsCholesky[i];
+        const Eigen::MatrixXd &L = precisionsCholesky[i];
+        const double log_det_precision = L.diagonal().array().log().sum();
 
-        const double log_det = cholesky.diagonal().array().log().sum();
+        Eigen::MatrixXd centered = data.rowwise() - mean.transpose();
+        Eigen::MatrixXd transformed = centered * L;
+        Eigen::VectorXd sq_norm = transformed.rowwise().squaredNorm();
 
-        Eigen::MatrixXd y = (data.rowwise() - mean.transpose()) * cholesky;
-
-        Eigen::VectorXd prob = y.rowwise().squaredNorm();
-
-        log_probabilities.col(i) = (-0.5 * (d * std::log(2 * M_PI) + prob.array()) +
-                                    log_det + std::log(result.weights[i])).matrix();
+        log_probabilities.col(i) = constant_term + log_det_precision
+                                   - 0.5 * sq_norm.array()
+                                   + std::log(result.weights[i]);
     }
 
     return log_probabilities;
